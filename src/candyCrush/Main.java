d27cf88459036181f99345011faae3e93b37783f
Camilo Herrera <camiloandherrera@gmail.com>
2020-06-28 22:40:23 -0500

    Creación de directorios y paquete.

---
+++ src/candyCrush/Main.java
@@ -0,0 +1,9 @@
+package candyCrush;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Yehehey ¿pero qué pasa chavales?¿todo bien, todo correcto? y yo que me alegro.");
+	}
+
+}
