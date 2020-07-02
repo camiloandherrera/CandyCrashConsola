80a8adecc821219d737dbff6e6168de5ca959fe3
Camilo Herrera <camiloandherrera@gmail.com>
2020-07-02 13:35:23 -0500

    Faltan, condiciones de victoria o derrota, recolección de jugadas y puntos

--- src/candyCrush/Main.java
+++ src/candyCrush/Main.java
@@ -1,70 +1,55 @@
 package candyCrush;
 import candyCrush.*;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// System.out.println("Yehehey �pero qu� pasa chavales?�todo bien, todo correcto? y yo que me alegro.");
-		Usuario usuario = new Usuario();
+	public static void main(String[] args){
 		
+		// Atributos del Main
+		Usuario usuario = new Usuario(); // Para ingresar el nombre del usuario
+		char presionadoNombre; // Lee un caracter presionado al momento de ingresar un nombre de usuario
+		Mundo mundo = new Mundo(10, 10, 0, 50); // Contiene datos del tablero y la partida en general
+		int validarInicial = 0; // Valida si se está generando un tablero inicial válido (sin jugadas ya listas)
+		Movimiento movimiento = new Movimiento(); // Contiene los movimientos y escaneos del tablero
+		Scanner ingreso = new Scanner(System.in); // Controla el input de usuario
+		
+		// Llamados, etc.
+		System.out.println("#### CANDY CRUSH: PALOS ####");
 		usuario.IngresarNombre();
+		String auxString = usuario.GetNombre().trim(); // Elimina nombres en blanco (espacios iniciales y finales, saltos de línea, etc)
+		usuario.SetNombre(auxString);
+		
+		if(usuario.GetNombre().length() <= 0) { // Genera el nombre de usuario por defecto si sólo se presionó Enter
+			usuario = new Usuario();
+		}
+		
+		do { // Inicia el tablero por primera vez
+			validarInicial = 0;
+			mundo.RellenarTablero();
+			validarInicial = movimiento.EscaneoInicial(mundo, mundo.GetTablero(), true);
+		}
+		while(validarInicial != 0);
 		
 		System.out.println("Nombre = " + usuario.GetNombre());
-		System.out.println("Vidas = " + usuario.GetVidas());
-		
-		/*String[][] tablero = new String[9][9];
-		tablero = rellenaTablero();
-		
-		pintaTablero(tablero);*/
-	}
-
-/*	static String[][] rellenaTablero()
-	{
-		String[][] tableroLleno = new String[9][9];
-		
-		for(int i = 0; i < 9; i++)
-		{
-			for(int j = 0; j < 9; j++)
-			{
-				Caramelo caramelo;
-				
-				if(i / 2 == 0)
-				{
-					caramelo = new Caramelo("Pica", "A");
-				}
-				else
-				{
-					 caramelo = new Caramelo("Joya", "B");
-				}
-				
-				tableroLleno[i][j] = caramelo.GetSimbolo();
-			}
-		}
-		
-		return tableroLleno;
-	}
-	
-	static void pintaTablero(String[][] tableroParaPintar)
-	{
-		for(int i = 0; i < 9; i++)
-		{
-			if(i == 0)
-			{
-				System.out.print("  1 2 3 4 5 6 7 8 9");
-			}
-			
+		System.out.println("\nVidas = " + usuario.GetVidas());
+		System.out.println("Movimientos restantes: " + mundo.GetMovimientos());
+		System.out.println("\nPuntos Totales: " + mundo.GetPuntos());
+		System.out.println("Puntos Jugada: " + mundo.GetPuntosJugada());
+		mundo.DibujarTablero();
+		int cont = 1; // ## TEMPORAL, DEBUG: Condición para seguir jugando ##
+		do {
 			System.out.println();
+			movimiento.SeleccionarCasilla();
+		
+			Casilla[][] auxiliarTablero;
+			auxiliarTablero = mundo.GetTablero();
 			
-			for(int j = 0; j < 9; j++)
-			{
-				if(j == 0)
-				{
-					System.out.print(i + 1 + " ");
-				}
-				
-				System.out.print(tableroParaPintar[i][j] + " ");
-			}
+			movimiento.SeleccionarDireccion(auxiliarTablero, mundo);
+			boolean hayJugada = movimiento.Verificar(movimiento.GetSelY(), movimiento.GetSelX(), auxiliarTablero, mundo);
+			
+			System.out.println("\n¿Hay jugada en la coordenada que se movió?: " + hayJugada);
 		}
+		while(cont == 1); 
 	}
-*/
 }
--- src/candyCrush/Mundo.java
+++ src/candyCrush/Mundo.java
@@ -1,9 +1,144 @@
 package candyCrush;
+import candyCrush.*;
+import java.lang.System;
 
 public class Mundo {
 	// Atributos
-	private int puntos = 0;
-	private int movimientos = 50;
+	// Tablero
+	private Casilla[][] tablero;
+	private int tamY;
+	private int tamX;
+	// Jugador/Partida
+	private int puntos;
+	private int puntosJugada; // Guarda los puntos de una jugada determinada
+	private int movimientos;
+	private boolean hayMovimientos;
 	
-
+	// Constructores
+	public Mundo(int tamY, int tamX, int puntos, int movimientos) {
+		this.tamY = tamY;
+		this.tamX = tamX;
+		this.tablero = new Casilla[this.tamY][this.tamX];
+		
+		this.puntos = puntos;
+		this.movimientos = movimientos;
+	}
+	
+	// Setters
+	public void SetTablero(Casilla[][] tablero) { // Establece el tablero de la partida
+		this.tablero = tablero;
+	}
+	
+	public void SetY(int tamY) { // Establece el límite en la coordenada en Y
+		this.tamY = tamY;
+	}
+	
+	public void SetX(int tamX) { // Establece el límite en la coordenada en X
+		this.tamX = tamX;
+	}
+	
+	public void SetPuntos(int puntos) { // Establece los puntos obtenidos en esta vida de jugador
+		this.puntos = puntos;
+	}
+	
+	public void SetPuntosJugada(int puntosJugada) { // Establece los puntos obtenidos de la última jugada posible
+		this.puntosJugada = puntosJugada;
+	}
+	
+	public void SetMovimientos(int movimientos) { // Establece los movimientos restantes en esta vida de jugador
+		this.movimientos = movimientos;
+	}
+	
+	public void SetHayMovimientos(boolean hayMovimientos) { // Establece si quedan o no movimientos en el tablero actual
+		this.hayMovimientos = hayMovimientos;
+	}
+	
+	// Getters
+	public Casilla[][]GetTablero() { // Obtiene el tablero de la partida
+		return this.tablero;
+	}
+	
+	public int GetY() { // Obtiene el límite de la coordenada Y
+		return this.tamY;
+	}
+	
+	public int GetX() { // Obtiene el límite de la coordenada X
+		return this.tamX;
+	}
+	
+	public int GetPuntos() { // Obtiene los puntos obtenidos en esta vida de jugador
+		return this.puntos;
+	}
+	
+	public int GetPuntosJugada() {  // Obtiene los puntos de la jugada más reciente
+		return this.puntosJugada;
+	}
+	
+	public int GetMovimientos() { // Obtiene los movimientos restantes en esta vida de jugador
+		return this.movimientos;
+	}
+	
+	public boolean GetHayMovimientos() { // Obtiene la condición de la existencia de movimientos;
+		return this.hayMovimientos;
+	}
+	
+	// Métodos
+	public void RestarMovimiento() { // Al hacer un movimiento, resta el contador de los mismos de esta vida de jugador
+		this.movimientos = this.movimientos - 1;
+	}
+	
+	public Caramelo CrearCaramelo() { // Genera los caramelos para rellenar el tablero
+		Caramelo caramelo;
+		int valorAleatorio = (int)Math.floor(Math.random()*5 + 1);
+		
+		switch(valorAleatorio) {
+		case 1:
+			caramelo = new Caramelo("Picas", "♠"); // Símbolo U+2660, "Palo de Picas negro"
+			break;
+		case 2:
+			caramelo = new Caramelo("Tréboles", "♣"); // Símbolo U+2663, "Palo de Tréboles negro"
+			break;
+		case 3:
+			caramelo = new Caramelo("Corazones", "♥"); // Símbolo U+2665, "Palo de Corazones negro"
+			break;
+		case 4:
+			caramelo = new Caramelo("Diamantes", "♦"); // Símbolo U+2666, "Palo de Diamantes negro"
+			break;
+		case 5:
+			caramelo = new Caramelo("Comodín", "☻"); // Símbolo U+263B, "Cara negra sonriente"
+			break;
+		default:
+			caramelo = new Caramelo("Picas", "♠"); // Símbolo U+2660, "Palo de Picas negro"
+			break;
+		}
+		return caramelo;
+	}
+	
+	public void RellenarTablero() { // Inicialmente llena el tablero con caramelos aleatorios
+		for (int i = 0; i < this.tamY; i++) {
+			for (int j = 0; j < this.tamX; j++) {
+				this.tablero[i][j] = new Casilla(i, j, CrearCaramelo());
+			}
+		}
+	}
+	
+	public void DibujarTablero() {
+		for (int i = 0; i < this.tamY; i++) {
+			if (i == 0) {
+				System.out.print("  ");
+				for (int m = 1; m <= this.tamX; m++) {
+					System.out.print(m + " ");
+				}
+			}
+			System.out.println();
+			
+			for (int j = 0; j < this.tamX; j++) {
+				if (j == 0) {
+					System.out.print(i + 1 + " ");
+				}
+				
+				System.out.print(this.tablero[i][j].GetContenido().GetSimbolo() + " ");
+			}
+		}
+	}
 }
